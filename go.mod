ded865a04b1df9f5433c0ca98345eb6962e38174
hanjiaxuan <597410004@qq.com>
2020-09-30 08:45:09 +0800

    第一版提交

---
+++ go.mod
@@ -0,0 +1,8 @@
+module gitbooktopdf
+
+go 1.15
+
+require (
+	github.com/antchfx/htmlquery v1.2.3
+	github.com/go-resty/resty/v2 v2.3.0
+)
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "gitbooktopdf/html"
+
+func main() {
+	//books := html.GetUrl()
+	//html.CrawlUrl(books)
+	html.ReplaceHtml()
+}
